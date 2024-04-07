68db97269b92bfa1643214204b571a9ffaf2e7fc
Larry Person <lp10011@gmail.com>
2024-04-07 16:42:17 -0400

    Adding some logging to help troubleshoot contact loading failures

--- src/extensions/job-runners/helpers.js
+++ src/extensions/job-runners/helpers.js
@@ -1,3 +1,5 @@
+/* eslint import/prefer-default-export:0 no-console:0 */
+
 import { r } from "../../server/models";
 
 export const saveJob = async (jobData, trx) => {
@@ -10,7 +12,16 @@ export const saveJob = async (jobData, trx) => {
     unsavedJob = { ...jobData, payload: JSON.stringify(jobData.payload || {}) };
   }
 
+  console.log("unsavedJob", unsavedJob);
+
+  console.log(
+    "SQL",
+    builder("job_request")
+      .insert(unsavedJob, "id")
+      .toString()
+  );
   const [res] = await builder("job_request").insert(unsavedJob, "id");
+  console.log("job_request insert return", res);
 
   return builder("job_request")
     .select("*")
